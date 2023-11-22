e6c07da18c05914f1ded27496856adbb0ccc847a
araidon <arai715work@gmail.com>
2023-11-22 15:41:08 +0900

    Implemented refactoring with GitHub Copilot

--- azure_invoice_analyzer.py
+++ azure_invoice_analyzer.py
@@ -4,37 +4,64 @@ import re
 import csv
 import sys
 
-csv_file = sys.argv[1]  # コマンドライン引数からAzureの請求書ファイル(csv)のパスを取得
 
-print(f"・ファイル読み込み開始: {datetime.datetime.now()}")
-df = pd.read_csv(csv_file, encoding="cp932", engine="pyarrow")
-print(f"・ファイル読み込み終了: {datetime.datetime.now()}")
+def read_csv_file(csv_file):
+    """
+    CSVファイルを読み込んでDataFrameを返す関数
+    """
+    print(f"・ファイル読み込み開始: {datetime.datetime.now()}")
+    df = pd.read_csv(csv_file, encoding="cp932", engine="pyarrow")
+    print(f"・ファイル読み込み終了: {datetime.datetime.now()}")
+    return df
 
-product_dict = {} #product・数量・コスト合計用の辞書
-product_dict2 = {} #サービス簡略化バージョン
 
-#print(f"・列ループ開始: {datetime.datetime.now()}")
-for product, quantity, cost,  in zip(df["製品 (Product)"],df["数量 (Quantity)"],df["コスト (Cost)"]):
-    product = re.sub(" - JA ..st","", product)
-    product_dict[product] = product_dict.get(product, {"数量":0.0, "コスト":0.0}) + cost
-    product_dict[product]["数量"] += quantity
-    product_dict[product]["コスト"] += cost
+def process_invoice(df):
+    """
+    請求書のデータを処理して製品ごとの数量と金額を計算する関数
+    """
+    product_dict = {}  # 製品・数量・コスト合計用の辞書
+    product_dict2 = {}  # サービス簡略化バージョン
 
-    product = re.sub(" -.*$","", product)
-    product_dict2[product] = product_dict2.get(product, 0) + cost
+    for product, quantity, cost in zip(df["製品 (Product)"], df["数量 (Quantity)"], df["コスト (Cost)"]):
+        # 製品名から不要な文字列を削除
+        product = re.sub(" - JA ..st", "", product)
 
-#print(f"・列ループ終了: {datetime.datetime.now()}")
-print(f"product種類: {len(product_dict)}")
-#print(f"金額合計: {sum(product_dict[0]['コスト'].values())}")
+        # 製品ごとの数量とコストを計算
+        product_dict.setdefault(product, {"数量": 0.0, "コスト": 0.0})
+        product_dict[product]["数量"] += quantity
+        product_dict[product]["コスト"] += cost
 
-def write_csv(filename: str, headerstr: str, data_dict):
-    f = open(filename, 'w', encoding='utf-8')
-    f.writelines(f"{headerstr}\n")
-    for key,value in data_dict.items():
-        if int(value) == 0: continue #0円はskip
-        f.writelines(f"{key},{int(value)}\n")
-    f.close()
+        # 製品名から不要な文字列を削除（サービス簡略化バージョン用）
+        product = re.sub(" -.*$", "", product)
+        product_dict2[product] = product_dict2.get(product, 0) + cost
 
-write_csv("dict1.csv","製品,数量,金額",product_dict)
-write_csv("dict2.csv","製品,金額",product_dict2)
+    return product_dict, product_dict2
 
+
+def write_csv(filename, headerstr, data_dict):
+    """
+    辞書のデータをCSVファイルに書き込む関数
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(f"{headerstr}\n")
+        for key, value in data_dict.items():
+            if int(value) == 0:
+                continue  # 0円はskip
+            f.write(f"{key},{int(value)}\n")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("引数が不足しています。Azureの請求書ファイル(csv)のパスを指定してください。")
+        sys.exit()
+
+    csv_file = sys.argv[1]  # コマンドライン引数からAzureの請求書ファイル(csv)のパスを取得
+
+    df = read_csv_file(csv_file)
+    product_dict, product_dict2 = process_invoice(df)
+
+    print(f"product種類: {len(product_dict)}")
+    total_cost = sum(value["コスト"] for value in product_dict.values())
+    print(f"金額合計: {total_cost}")
+
+    write_csv("dict2.csv", "製品,金額", product_dict2)
